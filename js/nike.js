b7f2d1c00f2b17b68ebe7c51d680cf9f466c5e7e
Pranali Dongare <139345001+dongarepranali@users.noreply.github.com>
2023-08-21 13:08:29 +0530

    final website

---
+++ js/nike.js
@@ -0,0 +1,22 @@
+function myFunction(x) {
+    x.classList.toggle("change");
+  }
+$('.shoes-slider').shoesSlider({
+    loop:true,
+    margin:30,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        },
+        1200:{
+            items:5
+        }
+    }
+})
